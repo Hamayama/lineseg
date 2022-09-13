747dc095ad53bd5ca70e0f0cb46e346ca98ac766
Hamayama <fkyo0985@gmail.com>
2022-09-14 08:50:12 +0900

    sample chg

--- sample-date-time.scm
+++ sample-date-time.scm
@@ -82,20 +82,17 @@
 (define work-start  (string->date "2000-01-01T08:00:00+0900" *date-format*))
 (define work-end    (string->date "2000-01-01T18:30:00+0900" *date-format*))
 
-
 ;; 各時間の範囲を作成
 (define day-seg     (make-date-lineseg `((,day-start   ,day-end))))
 (define lunch-seg   (make-date-lineseg `((,lunch-start ,lunch-end))))
 (define rest-seg    (make-date-lineseg `((,rest-start  ,rest-end))))
 (define work-seg    (make-date-lineseg `((,work-start  ,work-end))))
 
-
 ;; 各時間の長さを表示
 (print "day-time   = " (time-to-hour (lineseg-length day-seg))   " hr")
 (print "lunch-time = " (time-to-hour (lineseg-length lunch-seg)) " hr")
 (print "rest-time  = " (time-to-hour (lineseg-length rest-seg))  " hr")
 (print)
-
 
 ;; 実時間、定時内時間、定時外時間の長さを計算
 (define all-work-time     (lineseg-length (lineseg-subtract work-seg lunch-seg rest-seg)))
@@ -104,10 +101,8 @@
                                                             rest-seg)))
 (define extra-work-time   (time-sub all-work-time regular-work-time))
 
-
 ;; 実時間、定時内時間、定時外時間の長さを表示
 (print "all-work-time     = " (time-to-hour all-work-time)     " hr")
 (print "regular-work-time = " (time-to-hour regular-work-time) " hr")
 (print "extra-work-time   = " (time-to-hour extra-work-time)   " hr")
-
 
